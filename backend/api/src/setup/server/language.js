41afc918dbadb83a3678433806b5691b6ecb2777
Khanh <tran_khanh@outlook.com>
2020-11-10 01:36:00 +0700

    api language

---
+++ backend/api/src/setup/server/language.js
@@ -0,0 +1,14 @@
+import params from "../../setup/config/params.json";
+
+// Language middleware
+export default async function (request, response, next) {
+  let header = request.headers.language;
+
+  if (header && header !== null) {
+    request.language = header;
+  } else {
+    request.language = params.common.language.default;
+  }
+
+  next();
+}
