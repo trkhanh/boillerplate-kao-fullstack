f84f00c7a093452d083fea33217712bcc2a7a8cf
Khanh <tran_khanh@outlook.com>
2020-11-10 16:38:51 +0700

    update translate

---
+++ backend/api/src/setup/tranlsate/index.js
@@ -0,0 +1,11 @@
+// Imports
+import i18n from "i18n-js";
+
+// Languages
+import en from "./en.json";
+
+// Translate
+i18n.fallbacks = en;
+i18n.translations = { en /* es */ };
+
+export default i18n;
