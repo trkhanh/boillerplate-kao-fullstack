48b4e3d443241061fa7eb178536a173d8d0f47c8
Khanh <tran_khanh@outlook.com>
2020-11-10 18:11:13 +0700

    structure of laning page with Hemlmet config example

---
+++ frontend/landing/src/setup/config/env.js
@@ -0,0 +1,9 @@
+  
+// Configurations
+export const ENV = process.env.NODE_ENV
+
+export const URL_API = process.env.RAZZLE_URL_API
+export const URL_WEB = process.env.RAZZLE_URL_WEB
+export const URL_LANDING = process.env.RAZZLE_URL_LANDING
+
+export const GA_TRACKING_ID = process.env.RAZZLE_GA_TRACKING_ID
